3880bd70a787d4cb577f7b7e723d6cbd08021da5
esionascimento <esionascimento@proton.me>
2025-02-26 16:16:58 -0400

    refactor: main.dart funcionalidade abrir mapa, google maps ou maps.me, adicionarCoordenada

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +8,106 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: CoordenadasScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CoordenadasScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CoordenadasScreenState createState() => _CoordenadasScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CoordenadasScreenState extends State<CoordenadasScreen> {
+  final List<Map<String, double>> coordenadas = [];
+  final TextEditingController latitudeController = TextEditingController();
+  final TextEditingController longitudeController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void adicionarCoordenada() {
+    final double? lat = double.tryParse(latitudeController.text);
+    final double? lon = double.tryParse(longitudeController.text);
+
+    if (lat != null && lon != null) {
+      setState(() {
+        coordenadas.add({'latitude': lat, 'longitude': lon});
+      });
+
+      latitudeController.clear();
+      longitudeController.clear();
+    }
+  }
+
+  Future<void> abrirMapa(double lat, double lon) async {
+    final Uri mapsMeUri = Uri.parse("mapsme://map?ll=$lat,$lon&z=15");
+    final Uri googleMapsUri = Uri.parse(
+      "geo:$lat,$lon?q=$lat,$lon(Google Maps)",
+    );
+    final Uri webMapsUri = Uri.parse(
+      "https://www.google.com/maps/search/?api=1&query=$lat,$lon",
+    );
+
+    if (await canLaunch(mapsMeUri.toString())) {
+      await launchUrl(mapsMeUri, mode: LaunchMode.externalApplication);
+    }
+    if (await launchUrl(googleMapsUri, mode: LaunchMode.externalApplication)) {
+      return;
+    }
+    if (await launchUrl(webMapsUri, mode: LaunchMode.externalApplication)) {
+      return;
+    }
+
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text("Erro ao abrir o mapa.")));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: Text("Lista de Coordenadas")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            TextField(
+              controller: latitudeController,
+              decoration: InputDecoration(labelText: "Latitude"),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: longitudeController,
+              decoration: InputDecoration(labelText: "Longitude"),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: adicionarCoordenada,
+              child: Text("Adicionar Coordenada"),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: coordenadas.length,
+                itemBuilder: (context, index) {
+                  final coord = coordenadas[index];
+                  return ListTile(
+                    title: Text(
+                      "Lat: ${coord['latitude']}, Lon: ${coord['longitude']}",
+                    ),
+                    trailing: Icon(Icons.map),
+                    onTap:
+                        () =>
+                            abrirMapa(coord['latitude']!, coord['longitude']!),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
